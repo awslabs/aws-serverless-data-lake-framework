4cb33a15d04196aa999c42c9f07454b8a463f64e
jabaral <jabaral@amazon.com>
2021-01-20 10:22:20 -0500

    Better curl requests

--- thirdparty-scms/bbucket/functions.sh
+++ thirdparty-scms/bbucket/functions.sh
@@ -42,11 +42,11 @@ function bootstrap_repository_scm()
       cp -f ${SCM_DIR}/bitbucket-pipelines.yml ./bitbucket-pipelines.yml
     fi
     git add . 
-    git commit -m "Initial Commit" 2>&1
+    git commit -m "Initial Commit" > /dev/null
     BB_REPO=$(echo $REPOSITORY | awk '{print tolower($0)}' )
     echo "Creating repository ${PREFIX}-${REPOSITORY} on Bitbucket"
     set +e
-    OUTPUT=$(curl -X POST -H "Content-Type: application/json" -u $BBUSER:$APP_PASS  -d "{\"scm\": \"git\", \
+    OUTPUT=$(curl -sS -X POST -H "Content-Type: application/json" -u $BBUSER:$APP_PASS  -d "{\"scm\": \"git\", \
      \"is_private\": \"true\",\"project\": {\"key\": \"$BBPROJECT\"} }" \
      https://api.bitbucket.org/2.0/repositories/$WORKSPACE/$PREFIX-$BB_REPO)
     STATUS=$? && set -e
@@ -71,31 +71,50 @@ function setup_bitbucket_pipelines() {
     REMOTE_REPO=$REPOSITORY
     echo "Enabling pipeline ${PREFIX}-${REPOSITORY}"
     BB_REPO=$(echo $REPOSITORY | awk '{print tolower($0)}' )
-    curl -X PUT -H "Content-Type: application/json" -u ${BBUSER}:${APP_PASS} -d '{ "enabled": "true" }' https://api.bitbucket.org/2.0/repositories/$WORKSPACE/${PREFIX}-${BB_REPO}/pipelines_config 2>&1
-    echo "Creating REMOTE_REPO var"
-    curl -X POST -H "Content-Type: application/json" -u ${BBUSER}:${APP_PASS} -d "{ \"type\": \"pipeline_variable\",\"key\": \"REMOTE_REPO\", \"value\": \"$REMOTE_REPO\", \"secured\": \"false\" }" https://api.bitbucket.org/2.0/repositories/$WORKSPACE/${PREFIX}-${BB_REPO}/pipelines_config/variables/ 2>&1
+    curl -sS -X PUT -H "Content-Type: application/json" -u ${BBUSER}:${APP_PASS} -d '{ "enabled": "true" }' \
+    https://api.bitbucket.org/2.0/repositories/$WORKSPACE/${PREFIX}-${BB_REPO}/pipelines_config > /dev/null
+    echo "Creating pipelines variables"
+    curl -sS -X POST -H "Content-Type: application/json" -u ${BBUSER}:${APP_PASS} -d "{ \"type\": \"pipeline_variable\",\"key\": \"REMOTE_REPO\", \"value\": \"$REMOTE_REPO\", \"secured\": \"false\" }" \
+    https://api.bitbucket.org/2.0/repositories/$WORKSPACE/${PREFIX}-${BB_REPO}/pipelines_config/variables/ > /dev/null
     if [[ "${REPOSITORY}" == "sdlf-team" ]]; then #remove sdlf-team repositories creation
-      echo "Creating BBUSER & APP_PASS parameters for the ${PREFIX}-${BB_REPO} repository"
-      curl -X POST -H "Content-Type: application/json" -u ${BBUSER}:${APP_PASS} -d "{ \"type\": \"pipeline_variable\",\"key\": \"APP_PASS\", \"value\": \"$APP_PASS\", \"secured\": \"true\" }" https://api.bitbucket.org/2.0/repositories/$WORKSPACE/${PREFIX}-${BB_REPO}/pipelines_config/variables/ 2>&1
-      curl -X POST -H "Content-Type: application/json" -u ${BBUSER}:${APP_PASS} -d "{ \"type\": \"pipeline_variable\",\"key\": \"BBUSER\", \"value\": \"$BBUSER\", \"secured\": \"true\" }" https://api.bitbucket.org/2.0/repositories/$WORKSPACE/${PREFIX}-${BB_REPO}/pipelines_config/variables/ 2>&1
+      echo "Creating team variables for the repository:${PREFIX}-${BB_REPO}"
+      curl -sS -X POST -H "Content-Type: application/json" -u ${BBUSER}:${APP_PASS} -d "{ \"type\": \"pipeline_variable\",\"key\": \"APP_PASS\", \"value\": \"$APP_PASS\", \"secured\": \"true\" }" \
+      https://api.bitbucket.org/2.0/repositories/$WORKSPACE/${PREFIX}-${BB_REPO}/pipelines_config/variables/ > /dev/null
+      curl -sS -X POST -H "Content-Type: application/json" -u ${BBUSER}:${APP_PASS} -d "{ \"type\": \"pipeline_variable\",\"key\": \"BBUSER\", \"value\": \"$BBUSER\", \"secured\": \"true\" }" \
+      https://api.bitbucket.org/2.0/repositories/$WORKSPACE/${PREFIX}-${BB_REPO}/pipelines_config/variables/ > /dev/null
       echo "Creating PREFIX parameter"
-      curl -X POST -H "Content-Type: application/json" -u ${BBUSER}:${APP_PASS} -d "{ \"type\": \"pipeline_variable\",\"key\": \"PREFIX\", \"value\": \"$PREFIX\", \"secured\": \"false\" }" https://api.bitbucket.org/2.0/repositories/$WORKSPACE/${PREFIX}-${BB_REPO}/pipelines_config/variables/ 2>&1
+      curl -sS -X POST -H "Content-Type: application/json" -u ${BBUSER}:${APP_PASS} -d "{ \"type\": \"pipeline_variable\",\"key\": \"PREFIX\", \"value\": \"$PREFIX\", \"secured\": \"false\" }" \
+      https://api.bitbucket.org/2.0/repositories/$WORKSPACE/${PREFIX}-${BB_REPO}/pipelines_config/variables/ > /dev/null
     fi
     for branch in master test dev ; do
-      if [[ "$branch" == "test" ]]; then sleep 10; fi
-      echo Executing pipeline for $branch
-      curl -X POST -H "Content-Type: application/json" -u ${BBUSER}:${APP_PASS} -d "{\"target\": { \"ref_type\": \"branch\", \"type\": \"pipeline_ref_target\", \"ref_name\": \"$branch\" } }" https://api.bitbucket.org/2.0/repositories/$WORKSPACE/$PREFIX-$BB_REPO/pipelines/ 2>&1
+      echo "Executing pipeline for $branch"
+      OUTPUT=$(curl -sS -X POST -H "Content-Type: application/json" -u ${BBUSER}:${APP_PASS} -d "{\"target\": { \"ref_type\": \"branch\", \"type\": \"pipeline_ref_target\", \"ref_name\": \"$branch\" } }" \
+      https://api.bitbucket.org/2.0/repositories/$WORKSPACE/$PREFIX-$BB_REPO/pipelines/)
+      UUID=$(echo $OUTPUT | jq -r .uuid)
+      if [[  "$branch" == "master" ]]; then
+        if [[ "$UUID" != "" && "$UUID" != "null" ]]; then
+          for i in {1..12}; do # wait until 2 minutes. The master branch should be mirrored first
+            echo "Waiting pipeline on master to finish..." && sleep 10
+            STATE=$(curl -sS -H "Content-Type: application/json" -u ${BBUSER}:${APP_PASS} \
+            "https://api.bitbucket.org/2.0/repositories/$WORKSPACE/$PREFIX-$BB_REPO/pipelines/\{$UUID\}" |jq -r .state.name)
+            echo "The state of the pipeline with UUID $UUID of the repository $PREFIX-$BB_REPO is *$STATE*"
+            if [[ "$STATE" == "COMPLETED" ]]; then break; fi
+          done
+        else
+          echo "Error creating the pipeline. Check the output: $OUTPUT"
+        fi
+      fi
     done
 }
 
 function setup_bbucket_workspacevars() {
-
-  curl -X POST -H "Content-Type: application/json" -u ${BBUSER}:${APP_PASS} -d "{ \"type\": \"pipeline_variable\", \"key\": \"SDLF_AWS_ACCESS_KEY_ID\", \"value\": \"${SDLF_AWS_ACCESS_KEY_ID}\", \"secured\": \"true\" }" https://api.bitbucket.org/2.0/workspaces/$WORKSPACE/pipelines-config/variables 2>&1
-  curl -X POST -H "Content-Type: application/json" -u ${BBUSER}:${APP_PASS} -d "{ \"type\": \"pipeline_variable\", \"key\": \"SDLF_AWS_SECRET_ACCESS_KEY\", \"value\": \"${SDLF_AWS_SECRET_ACCESS_KEY}\", \"secured\": \"true\" }" https://api.bitbucket.org/2.0/workspaces/$WORKSPACE/pipelines-config/variables 2>&1
-  curl -X POST -H "Content-Type: application/json" -u ${BBUSER}:${APP_PASS} -d "{ \"type\": \"pipeline_variable\", \"key\": \"SDLF_AWS_SESSION_TOKEN\", \"value\": \"${SDLF_AWS_SESSION_TOKEN}\", \"secured\": \"true\" }" https://api.bitbucket.org/2.0/workspaces/$WORKSPACE/pipelines-config/variables 2>&1
-  curl -X POST -H "Content-Type: application/json" -u ${BBUSER}:${APP_PASS} -d "{ \"type\": \"pipeline_variable\", \"key\": \"SDLF_AWS_REGION\", \"value\": \"${REGION}\", \"secured\": \"false\" }" https://api.bitbucket.org/2.0/workspaces/$WORKSPACE/pipelines-config/variables 2>&1
-  curl -X POST -H "Content-Type: application/json" -u ${BBUSER}:${APP_PASS} -d "{ \"type\": \"pipeline_variable\", \"key\": \"SDLF_AWS_ROLE_TO_ASSUME_NAME\", \"value\": \"${SDLF_AWS_ROLE_TO_ASSUME_NAME}\", \"secured\": \"false\" }" https://api.bitbucket.org/2.0/workspaces/$WORKSPACE/pipelines-config/variables 2>&1
-  curl -X POST -H "Content-Type: application/json" -u ${BBUSER}:${APP_PASS} -d "{ \"type\": \"pipeline_variable\", \"key\": \"SDLF_AWS_ROLE_TO_ASSUME_ARN\", \"value\": \"${SDLF_AWS_ROLE_TO_ASSUME_ARN}\", \"secured\": \"false\" }" https://api.bitbucket.org/2.0/workspaces/$WORKSPACE/pipelines-config/variables 2>&1
+  echo "Creating workspace variables on BitBucket"
+  curl -sS -X POST -H "Content-Type: application/json" -u ${BBUSER}:${APP_PASS} -d "{ \"type\": \"pipeline_variable\", \"key\": \"SDLF_AWS_ACCESS_KEY_ID\", \"value\": \"${SDLF_AWS_ACCESS_KEY_ID}\", \"secured\": \"true\" }" https://api.bitbucket.org/2.0/workspaces/$WORKSPACE/pipelines-config/variables > /dev/null
+  curl -sS -X POST -H "Content-Type: application/json" -u ${BBUSER}:${APP_PASS} -d "{ \"type\": \"pipeline_variable\", \"key\": \"SDLF_AWS_SECRET_ACCESS_KEY\", \"value\": \"${SDLF_AWS_SECRET_ACCESS_KEY}\", \"secured\": \"true\" }" https://api.bitbucket.org/2.0/workspaces/$WORKSPACE/pipelines-config/variables > /dev/null
+  curl -sS -X POST -H "Content-Type: application/json" -u ${BBUSER}:${APP_PASS} -d "{ \"type\": \"pipeline_variable\", \"key\": \"SDLF_AWS_SESSION_TOKEN\", \"value\": \"${SDLF_AWS_SESSION_TOKEN}\", \"secured\": \"true\" }" https://api.bitbucket.org/2.0/workspaces/$WORKSPACE/pipelines-config/variables > /dev/null
+  curl -sS -X POST -H "Content-Type: application/json" -u ${BBUSER}:${APP_PASS} -d "{ \"type\": \"pipeline_variable\", \"key\": \"SDLF_AWS_REGION\", \"value\": \"${REGION}\", \"secured\": \"false\" }" https://api.bitbucket.org/2.0/workspaces/$WORKSPACE/pipelines-config/variables > /dev/null
+  curl -sS -X POST -H "Content-Type: application/json" -u ${BBUSER}:${APP_PASS} -d "{ \"type\": \"pipeline_variable\", \"key\": \"SDLF_AWS_ROLE_TO_ASSUME_NAME\", \"value\": \"${SDLF_AWS_ROLE_TO_ASSUME_NAME}\", \"secured\": \"false\" }" https://api.bitbucket.org/2.0/workspaces/$WORKSPACE/pipelines-config/variables > /dev/null
+  curl -sS -X POST -H "Content-Type: application/json" -u ${BBUSER}:${APP_PASS} -d "{ \"type\": \"pipeline_variable\", \"key\": \"SDLF_AWS_ROLE_TO_ASSUME_ARN\", \"value\": \"${SDLF_AWS_ROLE_TO_ASSUME_ARN}\", \"secured\": \"false\" }" https://api.bitbucket.org/2.0/workspaces/$WORKSPACE/pipelines-config/variables > /dev/null
 }
 
 function deploy_sdlf_foundations_scm() {
@@ -113,16 +132,12 @@ function deploy_sdlf_foundations_scm() {
     SDLF_AWS_ROLE_TO_ASSUME_NAME=$(jq -r '.[] | select(.ParameterKey=="sdlf_aws_role_to_assume_name") | .ParameterValue' ${SCM_DIR}/parameters.json)
     SDLF_AWS_ROLE_TO_ASSUME_ARN=$(jq -r '.[] | select(.ParameterKey=="sdlf_aws_role_to_assume_arn") | .ParameterValue' ${SCM_DIR}/parameters.json)
 
-    echo "click to continue..." && read -n 1
     setup_bbucket_workspacevars
 
     for REPOSITORY in "${REPOSITORIES[@]}"
     do
       echo =======================================================================
-      echo "bootstrap_repository_scm ${REPOSITORY}..."
-      echo "click to continue..." && read -n 1
       bootstrap_repository_scm $REPOSITORY
-      echo "setup_bitbucket_pipelines ${REPOSITORY}"
       setup_bitbucket_pipelines $REPOSITORY
     done
     cd "${DIRNAME}"
