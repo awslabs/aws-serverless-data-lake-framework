293ece1d9d6ce8663c3bcb5b08692879c5607458
Emmanuel Arenas Garcia <emmanuelarenasgarcia@gmail.com>
2021-10-25 23:44:06 -0500

    Fixing Merge conflicts

--- sdlf-utils/ingestion-examples/sqoop/sdlf-stageX/lambda/stage-x-create-emr-cluster/src/lambda_function.py
+++ sdlf-utils/ingestion-examples/sqoop/sdlf-stageX/lambda/stage-x-create-emr-cluster/src/lambda_function.py
@@ -142,7 +142,6 @@ def lambda_handler(event, context):
                     'Value': env
                 }
             ],
-            SecurityConfiguration=f'sdlf-{team}-emr-security-config',
             StepConcurrencyLevel=3)
 
     except Exception as e:
