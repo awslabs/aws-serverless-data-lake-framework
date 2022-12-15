797df51ea182eab3908d802e4c5a2327d9fe089d
Cyril Fait <cfait@misdre.info>
2022-12-15 14:07:04 +0100

    Add simple shell script and configuration files to help improve code quality

---
+++ validate.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -ex
+
+# python
+isort --check .
+black --check .
+flake8 .
+# pylint $(git ls-files --exclude-standard '*.py') # pylint is disabled for now
+trivy fs --security-checks vuln .
+
+# shell
+find . -not \( -path ./sdlf-utils -prune \) -type f \( -name '*.sh' -o -name '*.bash' -o -name '*.ksh' \) -print0 \
+| xargs -0 shellcheck -x --format gcc
+
+# cloudformation
+find . -not \( -path ./sdlf-utils -prune \) -type f -name '*.yaml' -print0 \
+| xargs -0 -L 1 cfn_nag_scan --deny-list-path .cfn-nag-deny-list.yml --input-path
