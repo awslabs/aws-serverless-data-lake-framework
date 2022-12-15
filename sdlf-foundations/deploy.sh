766d2e56d350bfbde6d88bb215a0012d42d259a5
Cyril Fait <cfait@misdre.info>
2022-12-15 15:04:20 +0100

    shellcheck

--- sdlf-foundations/deploy.sh
+++ sdlf-foundations/deploy.sh
@@ -12,7 +12,7 @@ usage () { echo "
     -s -- Name of S3 bucket to upload artifacts to
 "; }
 options=':n:p:s:h'
-while getopts $options option
+while getopts "$options" option
 do
     case "$option" in
         n  ) nflag=true; STACK_NAME=$OPTARG;;
@@ -25,84 +25,84 @@ do
     esac
 done
 
-if ! $pflag
+if ! "$pflag"
 then
     echo "-p not specified, using default..." >&2
     PROFILE="default"
 fi
-if ! $sflag
+if ! "$sflag"
 then
-    S3_BUCKET=$(sed -e 's/^"//' -e 's/"$//' <<<"$(aws ssm get-parameter --name /SDLF/S3/CFNBucket --profile $PROFILE --query "Parameter.Value")")
+    S3_BUCKET=$(sed -e 's/^"//' -e 's/"$//' <<<"$(aws ssm get-parameter --name /SDLF/S3/CFNBucket --profile "$PROFILE" --query "Parameter.Value")")
 fi
-if ! $nflag
+if ! "$nflag"
 then
     STACK_NAME="sdlf-foundations"
 fi
 
-ENV=$(sed -e 's/^"//' -e 's/"$//' <<<"$(aws ssm get-parameter --name /SDLF/Misc/pEnv --profile $PROFILE --query "Parameter.Value")")
+ENV=$(sed -e 's/^"//' -e 's/"$//' <<<"$(aws ssm get-parameter --name /SDLF/Misc/pEnv --profile "$PROFILE" --query "Parameter.Value")")
 
 echo "Checking if bucket exists ..."
-if ! aws s3 ls $S3_BUCKET --profile $PROFILE; then
+if ! aws s3 ls "$S3_BUCKET" --profile "$PROFILE"; then
   echo "S3 bucket named $S3_BUCKET does not exist. Create? [Y/N]"
-  read choice
-  if [ $choice == "Y" ] || [ $choice == "y" ]; then
-    aws s3 mb s3://$S3_BUCKET --profile $PROFILE
+  read -r choice
+  if [ "$choice" == "Y" ] || [ "$choice" == "y" ]; then
+    aws s3 mb s3://"$S3_BUCKET" --profile "$PROFILE"
   else
     echo "Bucket does not exist. Deploy aborted."
     exit 1
   fi
 fi
 
-mkdir $DIRNAME/output
-sam package --profile $PROFILE --template-file $DIRNAME/template.yaml --s3-bucket $S3_BUCKET --s3-prefix foundations --output-template-file $DIRNAME/output/packaged-template.yaml
+mkdir "$DIRNAME"/output
+sam package --profile "$PROFILE" --template-file "$DIRNAME"/template.yaml --s3-bucket "$S3_BUCKET" --s3-prefix foundations --output-template-file "$DIRNAME"/output/packaged-template.yaml
 
 echo "Checking if stack exists ..."
-if ! aws cloudformation describe-stacks --profile $PROFILE --stack-name $STACK_NAME; then
+if ! aws cloudformation describe-stacks --profile "$PROFILE" --stack-name "$STACK_NAME"; then
   echo -e "Stack does not exist, creating ..."
   aws cloudformation create-stack \
-    --stack-name $STACK_NAME \
-    --parameters file://$DIRNAME/parameters-$ENV.json \
-    --template-body file://$DIRNAME/output/packaged-template.yaml \
-    --tags file://$DIRNAME/tags.json \
+    --stack-name "$STACK_NAME" \
+    --parameters file://"$DIRNAME/parameters-$ENV".json \
+    --template-body file://"$DIRNAME"/output/packaged-template.yaml \
+    --tags file://"$DIRNAME"/tags.json \
     --capabilities "CAPABILITY_NAMED_IAM" "CAPABILITY_AUTO_EXPAND" \
-    --profile $PROFILE
+    --profile "$PROFILE"
 
   echo "Waiting for stack to be created ..."
-  aws cloudformation wait stack-create-complete --profile $PROFILE \
-    --stack-name $STACK_NAME
+  aws cloudformation wait stack-create-complete --profile "$PROFILE" \
+    --stack-name "$STACK_NAME"
 else
   echo -e "Stack exists, attempting update ..."
 
   set +e
   update_output=$( aws cloudformation update-stack \
-    --profile $PROFILE \
-    --stack-name $STACK_NAME \
-    --parameters file://$DIRNAME/parameters-$ENV.json \
-    --template-body file://$DIRNAME/output/packaged-template.yaml \
-    --tags file://$DIRNAME/tags.json \
+    --profile "$PROFILE" \
+    --stack-name "$STACK_NAME" \
+    --parameters file://"$DIRNAME/parameters-$ENV".json \
+    --template-body file://"$DIRNAME"/output/packaged-template.yaml \
+    --tags file://"$DIRNAME"/tags.json \
     --capabilities "CAPABILITY_NAMED_IAM" "CAPABILITY_AUTO_EXPAND" 2>&1)
   status=$?
   set -e
 
   echo "$update_output"
 
-  if [ $status -ne 0 ] ; then
+  if [ "$status" -ne 0 ] ; then
     # Don't fail for no-op update
     if [[ $update_output == *"ValidationError"* && $update_output == *"No updates"* ]] ; then
       echo -e "\nFinished create/update - no updates to be performed";
       exit 0;
     else
-      exit $status
+      exit "$status"
     fi
   fi
 
   echo "Waiting for stack update to complete ..."
-  aws cloudformation wait stack-update-complete --profile $PROFILE \
-    --stack-name $STACK_NAME 
+  aws cloudformation wait stack-update-complete --profile "$PROFILE" \
+    --stack-name "$STACK_NAME" 
   echo "Finished create/update successfully!"
 fi
 
 echo "Loading Deequ scripts ..."
-ARTIFACTS_BUCKET=$(sed -e 's/^"//' -e 's/"$//' <<<"$(aws ssm get-parameter --name /SDLF/S3/ArtifactsBucket --profile $PROFILE --query "Parameter.Value")")
-aws s3 cp ./scripts/deequ/jar/deequ-1.2.2-spark-2.4.jar s3://$ARTIFACTS_BUCKET/deequ/jars/ --profile $PROFILE
-aws s3 sync ./scripts/deequ/resources/ s3://$ARTIFACTS_BUCKET/deequ/scripts/ --profile $PROFILE
+ARTIFACTS_BUCKET=$(sed -e 's/^"//' -e 's/"$//' <<<"$(aws ssm get-parameter --name /SDLF/S3/ArtifactsBucket --profile "$PROFILE" --query "Parameter.Value")")
+aws s3 cp ./scripts/deequ/jar/deequ-1.2.2-spark-2.4.jar s3://"$ARTIFACTS_BUCKET"/deequ/jars/ --profile "$PROFILE"
+aws s3 sync ./scripts/deequ/resources/ s3://"$ARTIFACTS_BUCKET"/deequ/scripts/ --profile "$PROFILE"
--- sdlf-pipLibrary/build.sh
+++ sdlf-pipLibrary/build.sh
@@ -5,10 +5,10 @@ team_name=$1
 for dir in ./*/
 do
     dir=${dir%*/}      # remove the trailing "/"
-    echo ${dir##*/}    # print everything after the final "/"
+    echo "${dir##*/}"    # print everything after the final "/"
     echo "---- Looking to move to: "
 
-    cd $dir
+    cd "$dir"
     echo "Moving into dir..."
     echo "Current directory contents:"
     ls
@@ -19,33 +19,33 @@ do
         pip3 -q install -r ./requirements.txt -t layer/python
         cd layer/
         zip -r layer.zip python/ -x \*__pycache__\*
-        dir_name=$(echo "${dir//.\/}")
+        dir_name="${dir//.\/}"
         echo "Uploading Lambda Layer as sdlf-$team_name-$dir_name..."
         
         set +e
-        layer=$(aws lambda publish-layer-version --layer-name sdlf-$team_name-$dir_name --description "Contains the libraries specified in requirements.txt" --compatible-runtimes "python3.9" --zip-file fileb://./layer.zip)
+        layer=$(aws lambda publish-layer-version --layer-name sdlf-"$team_name-$dir_name" --description "Contains the libraries specified in requirements.txt" --compatible-runtimes "python3.9" --zip-file fileb://./layer.zip)
         status=$?
         set -e
 
-        if [ $status -ne 0 ] ; then
-            exit $status
+        if [ "$status" -ne 0 ] ; then
+            exit "$status"
         fi
         
-        latest_layer_version=$(echo $layer | jq -r .LayerVersionArn)
-        paramname=$(printf '/SDLF/Lambda/%s/%s' $team_name $dir_name)
-        aws ssm put-parameter --name $paramname --value $latest_layer_version --type String --overwrite
+        latest_layer_version=$(echo "$layer" | jq -r .LayerVersionArn)
+        paramname=$(printf '/SDLF/Lambda/%s/%s' "$team_name" "$dir_name")
+        aws ssm put-parameter --name "$paramname" --value "$latest_layer_version" --type String --overwrite
         cd ../..
 
     elif [ -f "./external_layers.json" ] || [ -f "./external_wheels.json" ]; then
         artifacts_bucket=$(aws ssm get-parameter --name /SDLF/S3/ArtifactsBucket --query "Parameter.Value" --output text)
         if [ -f "./external_layers.json" ]; then
             echo "external_layers.json exists"
-            python3 ../external_layers.py $artifacts_bucket $team_name
+            python3 ../external_layers.py "$artifacts_bucket" "$team_name"
         fi
         
         if [ -f "./external_wheels.json" ]; then
             echo "external_wheels.json exists"
-            python3 ../external_wheels.py $artifacts_bucket $team_name
+            python3 ../external_wheels.py "$artifacts_bucket" "$team_name"
         fi
         cd ../   
             
