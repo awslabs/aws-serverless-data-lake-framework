4c4c886d36fead758ba3c1af6bca0a53601fbda9
Cyril Fait <cfait@misdre.info>
2023-01-18 12:14:42 +0100

    add cfn-lint to validate.sh

--- validate.sh
+++ validate.sh
@@ -14,4 +14,6 @@ find . -not \( -path ./sdlf-utils -prune \) -type f \( -name '*.sh' -o -name '*.
 
 # cloudformation
 find . -not \( -path ./sdlf-utils -prune \) -type f -name '*.yaml' -print0 \
+| xargs -0 cfn-lint
+find . -not \( -path ./sdlf-utils -prune \) -type f -name '*.yaml' -print0 \
 | xargs -0 -L 1 cfn_nag_scan --deny-list-path .cfn-nag-deny-list.yml --input-path
