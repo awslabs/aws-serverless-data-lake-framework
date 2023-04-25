d17be629a448033044f4a7edeb6c15f85b59f551
Cyril Fait <cfait@misdre.info>
2023-04-25 11:42:10 +0200

    rewrite of SDLF deployment script
    from (roughly):
    -f (devops foundations in devops account)
    -o (roles in devops account for use by cicd pipelines in child accounts)
    -c (cicd resources in child accounts)
    
    to:
    crossaccount-cicd-roles (roles in child accounts for use by cicd pipelines in devops account)
    devops-account (devops/cicd resources in devops account)

--- deploy.sh
+++ deploy.sh
@@ -1,81 +1,38 @@
 #!/bin/bash
-sflag=false
-tflag=false
-rflag=false
-fflag=false
-cflag=false
-dflag=false
-xflag=false
-aflag=false
 
 DIRNAME=$PWD
 
+bold=$(tput bold)
+underline=$(tput smul)
+notunderline=$(tput rmul)
+notbold=$(tput sgr0)
+
+version () { echo "awssdlf/2.0.0"; }
+
 usage () { echo "
+    --version -- Prints the SDLF version
     -h -- Opens up this help message
-    -s -- Name of the AWS profile to use for the Shared DevOps Account
-    -t -- Name of the AWS profile to use for the Child Account
     -r -- AWS Region to deploy to (e.g. eu-west-1)
-    -f -- Deploys SDLF DevOps/CICD/Tooling in the AWS account of the profile provided with -s
-    -c -- Deploys Child Account CICD Resources used by SDLF DevOps/CICD/Tooling account
-    -d -- Domain accounts list
-    -x -- Deploys with an external git SCM. Allowed values: ado -> Azure DevOps, bb -> BitBucket, gitlab -> GitLab
-    -a -- Flag to add CodeCommit Pull Request test infrastructure
+
+    crossaccount-cicd-roles -d -p -- Deploys crossaccount IAM roles necessary for DevOps CICD pipelines
+        -d -- AWS account id of the Shared DevOps account
+        -p -- Name of the AWS profile to use where a SDLF data domain will reside
+    devops-account -d -p -- Deploys SDLF DevOps/CICD/Tooling resources
+        -d -- Comma-delimited list of AWS account ids where SDLF data domains are deployed
+        -p -- Name of the AWS profile to use where SDLF DevOps/CICD/Tooling will reside
+
+    Examples
+
+    ${bold}./deploy.sh${notbold} ${underline}crossaccount-cicd-roles${notunderline} ${bold}-d${notbold} ${underline}devops_aws_account_id${notunderline} ${bold}-p${notbold} ${underline}aws_account_profile${notunderline}
+    deploys IAM roles in the AWS account ${underline}aws_account_profile${notunderline} belongs to.
+    These roles are necessary for the DevOps account CICD pipelines to work.
+    They should be deployed first.
+
+    ${bold}./deploy.sh${notbold} ${underline}devops-account${notunderline} ${bold}-d${notbold} ${underline}domain_aws_account_id1,domain_aws_account_id2,...${notunderline} ${bold}-p${notbold} ${underline}devops_aws_account_profile${notunderline}
+    deploys SDLF DevOps/CICD/Tooling resources in the AWS account ${underline}devops_aws_account_profile${notunderline} belongs to.
+    These resources are used to deploy new data domains, teams, pipelines and datasets in the provided comma-delimited list of accounts.
+    This should be used after creating the crossaccount CICD roles with ${bold}./deploy.sh crossaccount-cicd-roles${notbold} for each of the AWS accounts listed.
 "; }
-options=':s:t:r:d:x:fcha'
-while getopts "$options" option
-do
-    case "$option" in
-        s  ) sflag=true; DEVOPS_PROFILE=${OPTARG};;
-        t  ) tflag=true; CHILD_PROFILE=${OPTARG};;
-        r  ) rflag=true; REGION=${OPTARG};;
-        d  ) dflag=true; DOMAIN_ACCOUNTS=${OPTARG};;
-        x  ) xflag=true; SCM=${OPTARG};;
-        f  ) fflag=true;;
-        c  ) cflag=true;;
-        a  ) aflag=true;;
-        h  ) usage; exit;;
-        \? ) echo "Unknown option: -$OPTARG" >&2; exit 1;;
-        :  ) echo "Missing option argument for -$OPTARG" >&2; exit 1;;
-        *  ) echo "Unimplemented option: -$OPTARG" >&2; exit 1;;
-    esac
-done
-
-# external SCMs config
-if "$xflag"
-then
-    # declare all the external SCMs supported for example: bitbucket github gitlab
-    # each one of these should have its directory, config and custom functions
-    declare -a SCMS=(ado bbucket gitlab)
-    # shellcheck disable=SC2199,SC2076
-    if [[ " ${SCMS[@]} " =~ " ${SCM} " ]]; then
-        SCM_DIR=${DIRNAME}/thirdparty-scms/${SCM}
-        # shellcheck source=thirdparty-scms/ado/functions.sh
-        source "$SCM_DIR"/functions.sh
-    else
-        echo SCM git value not valid: "$SCM". The allowed values are: "${SCMS[@]}"
-        exit 1
-    fi
-fi
-
-if ! "$sflag"
-then
-    echo "-s not specified, using default..." >&2
-    DEVOPS_PROFILE="default"
-fi
-if ! "$tflag"
-then
-    echo "-t not specified, using default..." >&2
-    CHILD_PROFILE="default"
-fi
-if ! "$rflag"
-then
-    echo "-r not specified, using default region..." >&2
-    REGION=$(aws configure get region --profile "$DEVOPS_PROFILE")
-fi
-
-DEVOPS_ACCOUNT=$(aws sts get-caller-identity --query 'Account' --output text --profile "$DEVOPS_PROFILE")
-CHILD_ACCOUNT=$(aws sts get-caller-identity --query 'Account' --output text --profile "$CHILD_PROFILE")
-AWS_PARTITION=$(aws sts get-caller-identity --query 'Arn' --output text --profile "$DEVOPS_PROFILE" | cut -d':' -f2)
 
 function template_protection()
 {
@@ -91,20 +48,99 @@ function template_protection()
         --profile "$CURRENT_PROFILE_NAME"
 }
 
-if "$fflag"
-then
-    echo "Deploying SDLF DevOps/Tooling (components repositories, CICD pipelines)" >&2
+crossaccount_cicd_roles () { 
+    pflag=false
+    rflag=false
+    dflag=false
+    options=':p:r:d:'
+    while getopts "$options" option
+    do
+        case "$option" in
+            p  ) pflag=true; DOMAIN_AWS_PROFILE=${OPTARG};;
+            r  ) rflag=true; REGION=${OPTARG};;
+            d  ) dflag=true; DEVOPS_ACCOUNT=${OPTARG};;
+            \? ) echo "Unknown option: -$OPTARG" >&2; exit 1;;
+            :  ) echo "Missing option argument for -$OPTARG" >&2; exit 1;;
+            *  ) echo "Unimplemented option: -$OPTARG" >&2; exit 1;;
+        esac
+    done
 
+    if ! "$pflag"
+    then
+        echo "-p not specified, using default..." >&2
+        DOMAIN_AWS_PROFILE="default"
+    fi
+    if ! "$rflag"
+    then
+        echo "-r not specified, using default region..." >&2
+        REGION=$(aws configure get region --profile "$DOMAIN_AWS_PROFILE")
+    fi
     if ! "$dflag"
     then
-        echo "Domain accounts need specifying" >&2
+        echo "-d must be specified when using the crossaccount-cicd-roles command" >&2
         exit 1
     fi
 
-    if "$xflag" ; then
-        echo "External SCM deployment detected: ${SCM}"
-        deploy_sdlf_foundations_scm
+    echo "Deploying SDLF DevOps/Tooling roles in data domain accounts" >&2
+
+    # Increase SSM Parameter Store throughput to 1,000 requests/second TODO
+    DOMAIN_ACCOUNT_ID=$(aws --profile "$DOMAIN_AWS_PROFILE" sts get-caller-identity --query 'Account' --output text)
+    AWS_PARTITION=$(aws --profile "$DOMAIN_AWS_PROFILE" sts get-caller-identity --query 'Arn' --output text | cut -d':' -f2)
+    aws --region "$REGION" --profile "$DOMAIN_AWS_PROFILE" ssm update-service-setting --setting-id arn:"$AWS_PARTITION":ssm:"$REGION:$DOMAIN_ACCOUNT_ID":servicesetting/ssm/parameter-store/high-throughput-enabled --setting-value true
+    DEVOPS_ARTIFACTS_BUCKET="sdlf-$REGION-$DEVOPS_ACCOUNT-cicd-cfn-artifacts"
+    DEVOPS_KMS_KEY_ALIAS="alias/sdlf-cicd-kms-key"
+    STACK_NAME=sdlf-cicd-domain-roles
+    aws cloudformation deploy \
+        --stack-name "$STACK_NAME" \
+        --template-file "$DIRNAME"/sdlf-cicd/template-cicd-domain-roles.yaml \
+        --parameter-overrides \
+            pDevOpsAccountId="$DEVOPS_ACCOUNT" \
+            pDevOpsArtifactsBucket="$DEVOPS_ARTIFACTS_BUCKET" \
+            pDevOpsKMSKey="$DEVOPS_KMS_KEY_ALIAS" \
+        --tags Framework=sdlf \
+        --capabilities "CAPABILITY_NAMED_IAM" "CAPABILITY_AUTO_EXPAND" \
+        --region "$REGION" \
+        --profile "$DOMAIN_AWS_PROFILE"
+
+    template_protection "$STACK_NAME" "$REGION" "$DOMAIN_AWS_PROFILE"
+
+    exit
+}
+
+devops_account () { 
+    pflag=false
+    rflag=false
+    dflag=false
+    options=':p:r:d:'
+    while getopts "$options" option
+    do
+        case "$option" in
+            p  ) pflag=true; DEVOPS_AWS_PROFILE=${OPTARG};;
+            r  ) rflag=true; REGION=${OPTARG};;
+            d  ) dflag=true; DOMAIN_ACCOUNTS=${OPTARG};;
+            \? ) echo "Unknown option: -$OPTARG" >&2; exit 1;;
+            :  ) echo "Missing option argument for -$OPTARG" >&2; exit 1;;
+            *  ) echo "Unimplemented option: -$OPTARG" >&2; exit 1;;
+        esac
+    done
+
+    if ! "$pflag"
+    then
+        echo "-p not specified, using default..." >&2
+        DEVOPS_AWS_PROFILE="default"
     fi
+    if ! "$rflag"
+    then
+        echo "-r not specified, using default region..." >&2
+        REGION=$(aws configure get region --profile "$DEVOPS_AWS_PROFILE")
+    fi
+    if ! "$dflag"
+    then
+        echo "-d must be specified when using the devops-account command" >&2
+        exit 1
+    fi
+
+    echo "Deploying SDLF DevOps/Tooling (components repositories, CICD pipelines)" >&2
 
     STACK_NAME=sdlf-cicd-prerequisites
     aws cloudformation deploy \
@@ -115,12 +151,17 @@ then
         --tags Framework=sdlf \
         --capabilities "CAPABILITY_NAMED_IAM" "CAPABILITY_AUTO_EXPAND" \
         --region "$REGION" \
-        --profile "$DEVOPS_PROFILE"
-    template_protection "$STACK_NAME" "$REGION" "$DEVOPS_PROFILE"
+        --profile "$DEVOPS_AWS_PROFILE"
+    template_protection "$STACK_NAME" "$REGION" "$DEVOPS_AWS_PROFILE"
 
-    ARTIFACTS_BUCKET=$(aws ssm get-parameter --name /SDLF/S3/DevOpsArtifactsBucket --query "Parameter.Value" --output text --region "$REGION" --profile "$DEVOPS_PROFILE")
+    ARTIFACTS_BUCKET=$(aws --region "$REGION" --profile "$DEVOPS_AWS_PROFILE" ssm get-parameter --name /SDLF/S3/DevOpsArtifactsBucket --query "Parameter.Value" --output text)
     mkdir "$DIRNAME"/output
-    aws cloudformation package --template-file "$DIRNAME"/sdlf-cicd/template-cicd-sdlf-repositories.yaml --s3-bucket "$ARTIFACTS_BUCKET" --s3-prefix template-cicd-sdlf-repositories --output-template-file "$DIRNAME"/output/packaged-template.yaml --region "$REGION" --profile "$DEVOPS_PROFILE"
+    aws cloudformation package \
+        --s3-bucket "$ARTIFACTS_BUCKET" --s3-prefix template-cicd-sdlf-repositories \
+        --template-file "$DIRNAME"/sdlf-cicd/template-cicd-sdlf-repositories.yaml \
+        --output-template-file "$DIRNAME"/output/packaged-template.yaml \
+        --region "$REGION" \
+        --profile "$DEVOPS_AWS_PROFILE"
     STACK_NAME=sdlf-cicd-sdlf-repositories
     aws cloudformation deploy \
         --s3-bucket "$ARTIFACTS_BUCKET" --s3-prefix sdlf-cicd-sdlf-repositories \
@@ -132,45 +173,47 @@ then
         --tags Framework=sdlf \
         --capabilities "CAPABILITY_NAMED_IAM" "CAPABILITY_AUTO_EXPAND" \
         --region "$REGION" \
-        --profile "$DEVOPS_PROFILE"
-    template_protection "$STACK_NAME" "$REGION" "$DEVOPS_PROFILE"
+        --profile "$DEVOPS_AWS_PROFILE"
+    template_protection "$STACK_NAME" "$REGION" "$DEVOPS_AWS_PROFILE"
 
     declare -a REPOSITORIES=("sdlf-cicd" "sdlf-foundations" "sdlf-team" "sdlf-pipeline" "sdlf-dataset" "sdlf-datalakeLibrary" "sdlf-pipLibrary" "sdlf-stageA" "sdlf-stageB")
     for REPOSITORY in "${REPOSITORIES[@]}"
     do
-        latest_commit=$(aws codecommit get-branch --repository-name "$REPOSITORY" --branch-name master --query 'branch.commitId' --output text --region "$REGION" --profile "$DEVOPS_PROFILE")
-        aws codecommit create-branch --repository-name "$REPOSITORY" --branch-name dev --commit-id "$latest_commit" --region "$REGION" --profile "$DEVOPS_PROFILE"
-        aws codecommit create-branch --repository-name "$REPOSITORY" --branch-name test --commit-id "$latest_commit" --region "$REGION" --profile "$DEVOPS_PROFILE"
+        latest_commit=$(aws --region "$REGION" --profile "$DEVOPS_AWS_PROFILE" codecommit get-branch --repository-name "$REPOSITORY" --branch-name master --query 'branch.commitId' --output text)
+        aws --region "$REGION" --profile "$DEVOPS_AWS_PROFILE" codecommit create-branch --repository-name "$REPOSITORY" --branch-name dev --commit-id "$latest_commit"
+        aws --region "$REGION" --profile "$DEVOPS_AWS_PROFILE" codecommit create-branch --repository-name "$REPOSITORY" --branch-name test --commit-id "$latest_commit"
     done
 
-    aws s3api put-object --bucket "$ARTIFACTS_BUCKET" --key sam-translate.py --body "$DIRNAME"/sdlf-cicd/sam-translate.py --profile "$DEVOPS_PROFILE"
-fi
+    aws --region "$REGION" --profile "$DEVOPS_AWS_PROFILE" s3api put-object --bucket "$ARTIFACTS_BUCKET" --key sam-translate.py --body "$DIRNAME"/sdlf-cicd/sam-translate.py
 
-if "$cflag"
+    exit
+}
+
+options='ha-:'
+while getopts "$options" option
+do
+    case "$option" in
+        -  )
+            case "${OPTARG}" in
+                version)
+                    version; exit;;
+            esac;;
+        h  ) usage; exit;;
+        \? ) echo "Unknown option: -$OPTARG" >&2; exit 1;;
+        *  ) echo "Unimplemented option: -$OPTARG" >&2; exit 1;;
+    esac
+done
+
+# crossaccount-cicd-roles, devops-account
+if [ "${1#-}" = "$1" ]
 then
-    echo "Deploying SDLF DevOps/Tooling roles in child accounts" >&2
-    # Increase SSM Parameter Store throughput to 1,000 requests/second TODO
-    aws ssm update-service-setting --setting-id arn:"$AWS_PARTITION":ssm:"$REGION:$CHILD_ACCOUNT":servicesetting/ssm/parameter-store/high-throughput-enabled --setting-value true --region "$REGION" --profile "$CHILD_PROFILE"
-    DEVOPS_ARTIFACTS_BUCKET=$(aws --region "$REGION" --profile "$DEVOPS_PROFILE" ssm get-parameter --name /SDLF/S3/DevOpsArtifactsBucket --query "Parameter.Value" --output text)
-    DEVOPS_KMS_KEY=$(aws --region "$REGION" --profile "$DEVOPS_PROFILE" ssm get-parameter --name /SDLF/KMS/CICDKeyId --query "Parameter.Value" --output text)
-    DEVOPS_BUILDCLOUDFORMATIONMODULESTAGE=$(aws --region "$REGION" --profile "$DEVOPS_PROFILE" ssm get-parameter --name /SDLF/CodeBuild/BuildCloudformationModuleStage --query "Parameter.Value" --output text)
-    DEVOPS_BUILDDEPLOYDATALAKELIBRARYLAYER=$(aws --region "$REGION" --profile "$DEVOPS_PROFILE" ssm get-parameter --name /SDLF/CodeBuild/BuildDeployDatalakeLibraryLayer --query "Parameter.Value" --output text)
-    DEVOPS_BUILDDEPLOYREQUIREMENTSLAYER=$(aws --region "$REGION" --profile "$DEVOPS_PROFILE" ssm get-parameter --name /SDLF/CodeBuild/BuildDeployRequirementsLayer --query "Parameter.Value" --output text)
-    STACK_NAME=sdlf-cicd-domain-roles
-    aws cloudformation deploy \
-        --stack-name "$STACK_NAME" \
-        --template-file "$DIRNAME"/sdlf-cicd/template-cicd-domain-roles.yaml \
-        --parameter-overrides \
-            pDevOpsAccountId="$DEVOPS_ACCOUNT" \
-            pDevOpsArtifactsBucket="$DEVOPS_ARTIFACTS_BUCKET" \
-            pDevOpsKMSKey="$DEVOPS_KMS_KEY" \
-            pBuildCloudformationModuleStage="$DEVOPS_BUILDCLOUDFORMATIONMODULESTAGE" \
-            pBuildDeployDatalakeLibraryLayer="$DEVOPS_BUILDDEPLOYDATALAKELIBRARYLAYER" \
-            pBuildDeployRequirementsLayer="$DEVOPS_BUILDDEPLOYREQUIREMENTSLAYER" \
-        --tags Framework=sdlf \
-        --capabilities "CAPABILITY_NAMED_IAM" "CAPABILITY_AUTO_EXPAND" \
-        --region "$REGION" \
-        --profile "$CHILD_PROFILE"
-
-    template_protection "$STACK_NAME" "$REGION" "$CHILD_PROFILE"
+    subcommand=$1
+    shift
+    unset OPTIND
 fi
+
+case "$subcommand" in
+    crossaccount-cicd-roles) crossaccount_cicd_roles "$@"; exit;;
+    devops-account) devops_account "$@"; exit;;
+    *) echo "Unimplemented command: $subcommand" >&2; exit 1;;
+esac
