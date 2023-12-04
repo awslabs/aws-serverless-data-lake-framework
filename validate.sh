0e047d73d0d0cafe615e4588657e68a8fd859889
Cyril Fait <cfait@misdre.info>
2023-12-04 13:35:33 +0100

    remove sdlf-utils exclusion in static-checking workflow
    fix all findings while we're at it

--- validate.sh
+++ validate.sh
@@ -8,12 +8,12 @@ ruff check .
 trivy fs --scanners vuln .
 
 # shell
-find . -not \( -path ./sdlf-utils -prune \) -type f \( -name '*.sh' -o -name '*.bash' -o -name '*.ksh' \) -print0 \
+find . -type f \( -name '*.sh' -o -name '*.bash' -o -name '*.ksh' \) -print0 \
 | xargs -0 shellcheck -x --format gcc
 
 # cloudformation
-find . -not \( -path ./sdlf-utils -prune \) -type f -name '*.yaml' -print0 \
+find . -type f -name '*.yaml' -print0 \
 | xargs -0 cfn-lint
 ## unfortunately cfn_nag doesn't support fn::foreach so we exclude files using it: https://github.com/stelligent/cfn_nag/issues/621
-find . -not \( -path ./sdlf-utils -prune -o -type f -name 'template-glue-job.yaml' -o -type f -name 'template-lambda-layer.yaml' \) -type f -name '*.yaml' -print0 \
+find . -not \( -type f -name 'template-glue-job.yaml' -o -type f -name 'template-lambda-layer.yaml' \) -type f -name '*.yaml' -print0 \
 | xargs -0 -L 1 cfn_nag_scan --fail-on-warnings --ignore-fatal --deny-list-path .cfn-nag-deny-list.yml --input-path
