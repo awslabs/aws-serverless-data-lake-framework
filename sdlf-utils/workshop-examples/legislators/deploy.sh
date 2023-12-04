ac97b62691fb38846ddc3788fd1b3446d23bf91b
Cyril Fait <cfait@misdre.info>
2023-12-04 13:35:33 +0100

    remove sdlf-utils exclusion in static-checking workflow
    fix all findings while we're at it

--- sdlf-utils/workshop-examples/legislators/deploy.sh
+++ sdlf-utils/workshop-examples/legislators/deploy.sh
@@ -11,7 +11,7 @@ usage () { echo "
     -s -- Name of S3 bucket to upload artifacts to
 "; }
 options=':p:s:h'
-while getopts $options option
+while getopts "$options" option
 do
     case "$option" in
         p  ) pflag=true; PROFILE=$OPTARG;;
@@ -23,99 +23,100 @@ do
     esac
 done
 
-if ! $pflag
+if ! "$pflag"
 then
     echo "-p not specified, using default..." >&2
     PROFILE="default"
 fi
-if ! $sflag
+REGION=$(aws configure get region --profile "$PROFILE")
+if ! "$sflag"
 then
-    S3_BUCKET=$(sed -e 's/^"//' -e 's/"$//' <<<"$(aws ssm get-parameter --name /SDLF/S3/ArtifactsBucket --profile $PROFILE --query "Parameter.Value")")
+    S3_BUCKET=$(aws --region "$REGION" --profile "$PROFILE" ssm get-parameter --name /SDLF/S3/ArtifactsBucket --query "Parameter.Value" --output text)
 fi
 
 echo "Checking if bucket exists ..."
-if ! aws s3 ls $S3_BUCKET --profile $PROFILE; then
+if ! aws s3 ls "$S3_BUCKET" --profile "$PROFILE"; then
   echo "S3 bucket named $S3_BUCKET does not exist. Create? [Y/N]"
-  read choice
-  if [ $choice == "Y" ] || [ $choice == "y" ]; then
-    aws s3 mb s3://$S3_BUCKET --profile $PROFILE
+  read -r choice
+  if [ "$choice" == "Y" ] || [ "$choice" == "y" ]; then
+    aws s3 mb s3://"$S3_BUCKET" --profile "$PROFILE"
   else
     echo "Bucket does not exist. Deploy aborted."
     exit 1
   fi
 fi
 
-ARTIFACTS_BUCKET=$(sed -e 's/^"//' -e 's/"$//' <<<"$(aws ssm get-parameter --name /SDLF/S3/ArtifactsBucket --profile $PROFILE --query "Parameter.Value")")
-aws s3 cp "$DIRNAME/scripts/legislators-glue-job.py" "s3://$ARTIFACTS_BUCKET/artifacts/" --profile $PROFILE
+ARTIFACTS_BUCKET=$(aws --region "$REGION" --profile "$PROFILE" ssm get-parameter --name /SDLF/S3/ArtifactsBucket --query "Parameter.Value" --output text)
+aws s3 cp "$DIRNAME/scripts/legislators-glue-job.py" "s3://$ARTIFACTS_BUCKET/artifacts/" --profile "$PROFILE"
 
-mkdir $DIRNAME/output
+mkdir "$DIRNAME"/output
 
 function send_legislators() 
 {
-  ORIGIN=$DIRNAME/data/
+  ORIGIN="$DIRNAME/data/"
   
-  CENTRAL_BUCKET=$(sed -e 's/^"//' -e 's/"$//' <<<"$(aws ssm get-parameter --name /SDLF/S3/CentralBucket --profile $PROFILE --query "Parameter.Value")")
-  STAGE_BUCKET=$(sed -e 's/^"//' -e 's/"$//' <<<"$(aws ssm get-parameter --name /SDLF/S3/StageBucket --profile $PROFILE --query "Parameter.Value")")
-  KMS_KEY=$(sed -e 's/^"//' -e 's/"$//' <<<"$(aws ssm get-parameter --name /SDLF/KMS/engineering/DataKeyId --profile $PROFILE --query "Parameter.Value")")
+  CENTRAL_BUCKET=$(aws --region "$REGION" --profile "$PROFILE" ssm get-parameter --name /SDLF/S3/CentralBucket --query "Parameter.Value" --output text)
+  STAGE_BUCKET=$(aws --region "$REGION" --profile "$PROFILE" ssm get-parameter --name /SDLF/S3/StageBucket --query "Parameter.Value" --output text)
+  KMS_KEY=$(aws --region "$REGION" --profile "$PROFILE" ssm get-parameter --name /SDLF/KMS/engineering/DataKeyId --query "Parameter.Value" --output text)
 
   S3_DESTINATION=s3://$CENTRAL_BUCKET/
   COUNT=0
-  for FILE in $(ls $ORIGIN | awk '{print $1}');
+  for FILE in "$ORIGIN"/*.json;
   do
-    let COUNT=COUNT+1
+    (( COUNT++ )) || true
     if [ "$CENTRAL_BUCKET" == "$STAGE_BUCKET" ];then
-      aws s3 cp $ORIGIN$FILE "${S3_DESTINATION}raw/engineering/legislators/" --profile $PROFILE --sse aws:kms --sse-kms-key-id $KMS_KEY
+      aws s3 cp "$ORIGIN$FILE" "${S3_DESTINATION}raw/engineering/legislators/" --profile "$PROFILE" --sse aws:kms --sse-kms-key-id "$KMS_KEY"
     else
-      aws s3 cp $ORIGIN$FILE "${S3_DESTINATION}engineering/legislators/" --profile $PROFILE --sse aws:kms --sse-kms-key-id $KMS_KEY
+      aws s3 cp "$ORIGIN$FILE" "${S3_DESTINATION}engineering/legislators/" --profile "$PROFILE" --sse aws:kms --sse-kms-key-id "$KMS_KEY"
     fi
     echo "transferred $COUNT files"
   done
 }
 
-aws cloudformation package --template-file $DIRNAME/scripts/legislators-glue-job.yaml \
-  --s3-bucket $S3_BUCKET \
-  --profile $PROFILE \
-  --output-template-file $DIRNAME/output/packaged-template.yaml
+aws cloudformation package --template-file "$DIRNAME"/scripts/legislators-glue-job.yaml \
+  --s3-bucket "$S3_BUCKET" \
+  --profile "$PROFILE" \
+  --output-template-file "$DIRNAME"/output/packaged-template.yaml
 
-if ! aws cloudformation describe-stacks --profile $PROFILE --stack-name $STACK_NAME; then
+if ! aws cloudformation describe-stacks --profile "$PROFILE" --stack-name "$STACK_NAME"; then
   echo -e "Stack does not exist, creating ..."
   aws cloudformation create-stack \
-  --profile $PROFILE \
-  --stack-name $STACK_NAME \
-  --template-body file://$DIRNAME/output/packaged-template.yaml \
+  --profile "$PROFILE" \
+  --stack-name "$STACK_NAME" \
+  --template-body file://"$DIRNAME"/output/packaged-template.yaml \
   --capabilities "CAPABILITY_NAMED_IAM" "CAPABILITY_AUTO_EXPAND"
 
   echo "Waiting for stack to be created ..."
-  aws cloudformation wait stack-create-complete --profile $PROFILE \
-    --stack-name $STACK_NAME
+  aws cloudformation wait stack-create-complete --profile "$PROFILE" \
+    --stack-name "$STACK_NAME"
   send_legislators
 else
   echo -e "Stack exists, attempting update ..."
 
   set +e
   update_output=$(aws cloudformation update-stack \
-    --profile $PROFILE \
-    --stack-name $STACK_NAME \
-    --template-body file://$DIRNAME/output/packaged-template.yaml \
+    --profile "$PROFILE" \
+    --stack-name "$STACK_NAME" \
+    --template-body file://"$DIRNAME"/output/packaged-template.yaml \
     --capabilities "CAPABILITY_NAMED_IAM" "CAPABILITY_AUTO_EXPAND" 2>&1)
   status=$?
   set -e
 
   echo "$update_output"
 
-  if [ $status -ne 0 ] ; then
+  if [ "$status" -ne 0 ] ; then
     # Don't fail for no-op update
     if [[ $update_output == *"ValidationError"* && $update_output == *"No updates"* ]] ; then
       echo -e "\nFinished create/update - no updates to be performed";
       send_legislators;
       exit 0;
     else
-      exit $status
+      exit "$status"
     fi
   fi
 
   echo "Waiting for stack update to complete ..."
-  aws cloudformation wait stack-update-complete --profile $PROFILE \
-    --stack-name $STACK_NAME 
+  aws cloudformation wait stack-update-complete --profile "$PROFILE" \
+    --stack-name "$STACK_NAME"
   echo "Finished create/update successfully!"
 fi
