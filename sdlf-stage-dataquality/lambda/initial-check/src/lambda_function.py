a5793ab44fa15a7a7ae5d9d8ad7e0ef6ac6a8b23
Cyril Fait <cfait@misdre.info>
2024-01-03 14:07:04 +0100

    rewrite stage-dataquality stepfunctions state machine
    remove data-quality-controller job, run suggestions and verification jobs directly from the state machine
    remove crawl-data lambda, run glue crawler directly from the state machine
    remove check-job lambda, the state-machine handles it with glue .sync integration
    better lambda error handling
    
    remove dedicated data quality bucket, use central bucket/stage bucket instead
    remove dedicated glue crawler, use crawler defined in sdlf-dataset instead
    
    optional vpc support: specify boto3 client endpoint, vpc config for lambda functions

--- sdlf-stage-dataquality/lambda/initial-check/src/lambda_function.py
+++ sdlf-stage-dataquality/lambda/initial-check/src/lambda_function.py
@@ -2,20 +2,87 @@ import datetime as dt
 import json
 import logging
 import time
+import os
 
 import boto3
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+from datalake_library import octagon
+from datalake_library.commons import init_logger
+from datalake_library.configuration.resource_configs import DynamoConfiguration
+from datalake_library.interfaces.dynamo_interface import DynamoInterface
 
-dynamodb = boto3.resource("dynamodb")
-ssm = boto3.client("ssm")
-glue = boto3.client("glue")
+logger = init_logger(__name__)
+
+dynamodb = boto3.client("dynamodb")
+ssm_endpoint_url = "https://ssm." + os.getenv("AWS_REGION") + ".amazonaws.com"
+ssm = boto3.client("ssm", endpoint_url=ssm_endpoint_url)
+glue_endpoint_url = "https://glue." + os.getenv("AWS_REGION") + ".amazonaws.com"
+glue = boto3.client("glue", endpoint_url=glue_endpoint_url)
 
 
-def datetimeconverter(o):
-    if isinstance(o, dt.datetime):
-        return o.__str__()
+def get_dataquality_enabled_tables(team, dataset, env, tables):
+    schemas_table = f"octagon-DataSchemas-{env}"
+    dataquality_tables = []
+    for table in tables:
+        table_item = dynamodb.get_item(TableName=schemas_table, Key={"name": f"{team}-{dataset}-{table}"})
+        logger.info(table_item)
+        if "Item" not in table_item:
+            # In case replication Lambda is in the process of writing the item
+            time.sleep(180)
+            table_item = dynamodb.get_item(TableName=schemas_table, Key={"name": f"{team}-{dataset}-{table}"})
+
+        if table_item["Item"]["data_quality_enabled"] == "Y":
+            dataquality_tables.append(table)
+
+    return dataquality_tables
+
+def get_glue_transform_details(bucket, team, dataset, env, pipeline, stage, glue_tables):
+    dynamo_config = DynamoConfiguration()
+    dynamo_interface = DynamoInterface(dynamo_config)
+
+    transform_info = dynamo_interface.get_transform_table_item(f"{team}-{dataset}")
+    suggestions_table = f"octagon-DataQualitySuggestions-{env}"
+
+    suggestions_job_name = f"sdlf-{team}-{dataset}-data-quality-suggestions"
+    verification_job_name = f"sdlf-{team}-{dataset}-data-quality-verification"
+    glue_database = ssm.get_parameter(Name=f"/SDLF/Glue/{team}/{dataset}/DataCatalog")["Parameter"]["Value"]
+    glue_capacity = {"WorkerType": "G.1X", "NumberOfWorkers": 10}
+    glue_suggestions_arguments = glue_verification_arguments = {
+        "--team": team,
+        "--dataset": dataset,
+        "--glueDatabase": glue_database,
+        "--glueTables": "",
+    }
+
+    suggestions_tables = []
+    verification_tables = []
+    for table in glue_tables:
+        suggestions_item = dynamodb.query(TableName=suggestions_table, IndexName="table-index", KeyConditionExpression=Key("table_hash_key").eq(f"{team}-{dataset}-{table}"))["Items"]
+        if suggestions_item:
+            verification_tables.append(table)
+        else:
+            suggestions_tables.append(table)
+
+    if suggestions_tables:
+        glue_suggestions_arguments["--glueTables"] = ",".join(suggestions_tables)
+
+    if verification_tables:
+        glue_verification_arguments["--glueTables"] = ",".join(verification_tables)
+
+    logger.info(f"Pipeline is {pipeline}, stage is {stage}")
+    if pipeline in transform_info.get("pipeline", {}):
+        if stage in transform_info["pipeline"][pipeline]:
+            logger.info(f"Details from DynamoDB: {transform_info['pipeline'][pipeline][stage]}")
+            suggestions_job_name = transform_info["pipeline"][pipeline][stage].get("suggestions_job_name", suggestions_job_name)
+            verification_job_name = transform_info["pipeline"][pipeline][stage].get("verification_job_name", verification_job_name)
+            glue_capacity = transform_info["pipeline"][pipeline][stage].get("glue_capacity", glue_capacity)
+            glue_suggestions_arguments |= transform_info["pipeline"][pipeline][stage].get("glue_extra_arguments", {})
+            glue_verification_arguments |= transform_info["pipeline"][pipeline][stage].get("glue_extra_arguments", {})
+
+    return {"suggestions_job_name": suggestions_job_name, "verification_job_name": verification_job_name,
+            "suggestions_arguments": glue_suggestions_arguments,
+            "verification_arguments": glue_verification_arguments,
+            **glue_capacity}
 
 
 def lambda_handler(event, context):
@@ -30,46 +97,23 @@ def lambda_handler(event, context):
     """
     try:
         logger.info("Fetching event data from previous step")
+        bucket = event["body"]["bucket"]
         team = event["body"]["team"]
+        pipeline = event["body"]["pipeline"]
+        stage = event["body"]["pipeline_stage"]
         dataset = event["body"]["dataset"]
         tables = event["body"]["job"]["jobDetails"]["tables"]
         env = event["body"]["env"]
 
-        job_name = "sdlf-data-quality-controller"
-        response = {"job": {"jobName": job_name, "jobStatus": "PASS"}}
-
         # Checking if Data Quality is enabled on tables
         logger.info("Querying data quality enabled tables")
-        schemas_table = dynamodb.Table("octagon-DataSchemas-{}".format(env))
+        dataquality_tables = get_dataquality_enabled_tables(team, dataset, env, tables)
+        logger.info(dataquality_tables)
+        event["body"]["glue"] = get_glue_transform_details(bucket, team, dataset, env, pipeline, stage)
+        event["body"]["glue"]["crawler_name"] = "-".join(["sdlf", team, dataset, "post-stage-crawler"])
+        logger.info(event["body"]["glue"])
 
-        deequ_tables = []
-        for table in tables:
-            table_item = schemas_table.get_item(Key={"name": f"{team}-{dataset}-{table}"})
-            if "Item" not in table_item:
-                # In case replication Lambda is in the process of writing the item
-                time.sleep(180)
-                table_item = schemas_table.get_item(Key={"name": f"{team}-{dataset}-{table}"})
-
-            if table_item["Item"]["data_quality_enabled"] == "Y":
-                deequ_tables.append(table)
-
-        if deequ_tables:
-            logger.info("Running Data Quality Controller Glue Job")
-            job_response = glue.start_job_run(
-                JobName=job_name,
-                Arguments={
-                    "--team": team,
-                    "--dataset": dataset,
-                    "--glueDatabase": ssm.get_parameter(Name=f"/SDLF/Glue/{team}/{dataset}/DataCatalog")["Parameter"][
-                        "Value"
-                    ],
-                    "--glueTables": ",".join(deequ_tables),
-                },
-            )
-            json_data = json.loads(json.dumps(job_response, default=datetimeconverter))
-            response["job"]["jobRunId"] = json_data.get("JobRunId")
-            response["job"]["jobStatus"] = "STARTED"
     except Exception as e:
         logger.error("Fatal error", exc_info=True)
         raise e
-    return response
+    return event
