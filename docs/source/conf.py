34e9d6dbd325abd23ecf8ca1bc4384f6ba6c9d92
Cyril Fait <cfait@misdre.info>
2024-02-09 13:38:06 +0100

    update docs conf, replace recommonmark with myst_parser

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,12 +18,12 @@ import sphinx_bootstrap_theme
 # -- Project information -----------------------------------------------------
 
 project = "AWS Serverless Data Lake Framework"
-author = "Abdel Jaidi"
+author = "Abdel Jaidi, Cyril Fait"
 
 master_doc = "index"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.1"
+release = "2.0.0"
 
 
 # -- General configuration ---------------------------------------------------
@@ -31,7 +31,7 @@ release = "0.0.1"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autosectionlabel", "sphinx.ext.autosummary", "sphinx.ext.napoleon", "recommonmark"]
+extensions = ["sphinx.ext.autosectionlabel", "sphinx.ext.autosummary", "sphinx.ext.napoleon", "myst_parser"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
