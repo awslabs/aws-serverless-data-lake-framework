d12d3dbbfa8e66732e343ad04a0b4c01c7e104dc
Cyril Fait <cfait@misdre.info>
2024-03-25 11:59:29 +0100

    kms key aliases

--- deploy.sh
+++ deploy.sh
@@ -88,7 +88,7 @@ crossaccount_cicd_roles () {
     AWS_PARTITION=$(aws --profile "$DOMAIN_AWS_PROFILE" sts get-caller-identity --query 'Arn' --output text | cut -d':' -f2)
     aws --region "$REGION" --profile "$DOMAIN_AWS_PROFILE" ssm update-service-setting --setting-id "arn:$AWS_PARTITION:ssm:$REGION:$DOMAIN_ACCOUNT_ID:servicesetting/ssm/parameter-store/high-throughput-enabled" --setting-value true
     DEVOPS_ARTIFACTS_BUCKET="sdlf-$REGION-$DEVOPS_ACCOUNT-cicd-cfn-artifacts"
-    DEVOPS_KMS_KEY_ALIAS="alias/sdlf-cicd-kms-key"
+    DEVOPS_KMS_KEY_ALIAS="alias/sdlf2-cicd-kms-key"
     STACK_NAME=sdlf-cicd-domain-roles
     aws cloudformation deploy \
         --stack-name "$STACK_NAME" \
