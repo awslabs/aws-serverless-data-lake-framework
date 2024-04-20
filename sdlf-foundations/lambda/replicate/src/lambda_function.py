b018643aa86ba9e61da0ca8a1c1822a804bb3618
Cyril Fait <cfait@misdre.info>
2024-04-21 01:30:15 +0200

    use proper DynamoDB JSON structure for items in sdlf-foundations replicate lambda

--- sdlf-foundations/lambda/replicate/src/lambda_function.py
+++ sdlf-foundations/lambda/replicate/src/lambda_function.py
@@ -1,7 +1,7 @@
 import json
 import logging
 import os
-from datetime import datetime
+from datetime import UTC, datetime
 
 import boto3
 
@@ -19,7 +19,7 @@ schemas_table = ssm.get_parameter(Name="/SDLF/Dynamo/DataSchemas")["Parameter"][
 
 
 def get_current_time():
-    return datetime.now(datetime.UTC).strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + "Z"
+    return datetime.now(UTC).isoformat()
 
 
 def grant_table_permissions(iam_arn, database, table, permissions):
@@ -37,33 +37,30 @@ def grant_table_permissions(iam_arn, database, table, permissions):
             Resource={"Table": {"DatabaseName": database, "Name": table}},
             Permissions=permissions,
         )
-        logger.info("{} Permissions granted to {} for table {}.{}".format(permissions, iam_arn, database, table))
+        logger.info(f"{permissions} Permissions granted to {iam_arn} for table {database}.{table}")
     except Exception as e:
-        logger.error(
-            "Error granting {} permissions to {} for table {}.{} . {}".format(permissions, iam_arn, database, table, e)
-        )
+        logger.error(f"Error granting {permissions} permissions to {iam_arn} for table {database}.{table} . {e}")
 
 
 def build_table_item(team, dataset, table):
     table_item = {}
-    table_item["created_at"] = get_current_time()
-    table_item["updated_at"] = get_current_time()
-    table_item["team"] = team
-    table_item["dataset"] = dataset
-    table_item["table"] = table["Name"]
-    table_item["name"] = "{}-{}-{}".format(team, dataset, table["Name"])
-    table_item["glue_table"] = table["Name"]
-    table_item["glue_database"] = table["DatabaseName"]
-    table_item["status"] = "ACTIVE"
-    table_item["type"] = table["TableType"]
-    table_item["schema"] = sorted(table["StorageDescriptor"]["Columns"], key=lambda i: i["Name"])
-    table_item["schema_version"] = 0
-    table_item["data_quality_enabled"] = "Y"
+    table_item["created_at"] = {"S": get_current_time()}
+    table_item["updated_at"] = {"S": get_current_time()}
+    table_item["team"] = {"S": team}
+    table_item["dataset"] = {"S": dataset}
+    table_item["table"] = {"S": table["Name"]}
+    table_item["name"] = {"S": f"{team}-{dataset}-{table["Name"]}"}
+    table_item["glue_table"] = {"S": table["Name"]}
+    table_item["glue_database"] = {"S": table["DatabaseName"]}
+    table_item["status"] = {"S": "ACTIVE"}
+    table_item["type"] = {"S": table["TableType"]}
+    table_item["schema"] = {"S": str(sorted(table["StorageDescriptor"]["Columns"], key=lambda i: i["Name"]))}
+    table_item["schema_version"] = {"N": "0"}
     return table_item
 
 
 def get_table_item(table_id):
-    response = dynamodb.get_item(TableName=schemas_table, Key={"name": table_id})
+    response = dynamodb.get_item(TableName=schemas_table, Key={"name": {"S": table_id}})
     return response["Item"]
 
 
@@ -75,16 +72,16 @@ def put_table_item(table_item):
 def update_table_item(table_id, schema):
     dynamodb.update_item(
         TableName=schemas_table,
-        Key={"name": table_id},
+        Key={"name": {"S": table_id}},
         UpdateExpression="set #s=:s, updated_at=:t, schema_version=schema_version+:val",
-        ExpressionAttributeValues={":s": schema, ":t": get_current_time(), ":val": 1},
+        ExpressionAttributeValues={":s": {"S": schema}, ":t": {"S": get_current_time()}, ":val": {"N": "1"}},
         ExpressionAttributeNames={"#s": "schema"},
         ReturnValues="UPDATED_NEW",
     )
 
 
 def delete_table_item(table_id):
-    response = dynamodb.delete_item(TableName=schemas_table, Key={"name": table_id})
+    response = dynamodb.delete_item(TableName=schemas_table, Key={"name": {"S": table_id}})
     return response
 
 
@@ -104,12 +101,12 @@ def lambda_handler(event, context):
         team = database_name.split("_")[-3]
         dataset = database_name.split("_")[-2]
 
-        logger.info("Processing {} change on {} dataset".format(type_of_change, dataset))
+        logger.info(f"Processing {type_of_change} change on {dataset} dataset (team: {team})")
 
         if type_of_change in ["CreateTable"]:
             changed_tables = event["detail"]["changedTables"]
             for table_name in changed_tables:
-                logger.info("Processing table: {}".format(table_name))
+                logger.info(f"Processing table: {table_name}")
                 try:
                     table = glue.get_table(DatabaseName=database_name, Name=table_name)["Table"]
                     table_item = build_table_item(team, dataset, table)
@@ -117,29 +114,29 @@ def lambda_handler(event, context):
                     iam_arn = ssm.get_parameter(Name="/SDLF/IAM/DataLakeAdminRoleArn")["Parameter"]["Value"]
                     grant_table_permissions(iam_arn, database_name, table_name, ["SELECT", "ALTER", "INSERT", "DELETE"])
                 except Exception as e:
-                    logger.error("Fatal error for table {} in database {}".format(table_name, database_name))
+                    logger.error(f"Fatal error for table {table_name} in database {database_name}")
                     logger.error(e)
                     pass  # Pass to unblock valid tables
 
         elif type_of_change in ["DeleteTable", "BatchDeleteTable"]:
             changed_tables = event["detail"]["changedTables"]
             for table_name in changed_tables:
-                logger.info("Processing table: {}".format(table_name))
-                table_id = "{}-{}-{}".format(team, dataset, table_name)
+                logger.info(f"Processing table: {table_name}")
+                table_id = f"{team}-{dataset}-{table_name}"
                 delete_table_item(table_id)
 
         elif type_of_change in ["UpdateTable"]:
             table_name = event["detail"]["tableName"]
-            logger.info("Processing table: {}".format(table_name))
-            table_id = "{}-{}-{}".format(team, dataset, table_name)
+            logger.info(f"Processing table: {table_name}")
+            table_id = f"{team}-{dataset}-{table_name}"
             table = glue.get_table(DatabaseName=database_name, Name=table_name)["Table"]
-            new_schema = sorted(table["StorageDescriptor"]["Columns"], key=lambda i: i["Name"])
+            new_schema = str(sorted(table["StorageDescriptor"]["Columns"], key=lambda i: i["Name"]))
             current_item = get_table_item(table_id)
             if current_item["schema"] != new_schema:
                 update_table_item(table_id, new_schema)
 
         else:
-            logger.info("Unsupported {} operation".format(type_of_change))
+            logger.info(f"Unsupported {type_of_change} operation")
     except Exception as e:
         logger.error("Fatal error", exc_info=True)
         raise e
