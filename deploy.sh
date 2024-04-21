ba90458c7b2b7e86db5c60baa630643a4273ffc9
Cyril Fait <cfait@misdre.info>
2024-04-21 18:27:27 +0200

    update user agent

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ underline=$(tput smul)
 notunderline=$(tput rmul)
 notbold=$(tput sgr0)
 
-version () { echo "awssdlf/2.1.0"; }
+version () { echo "awssdlf/2.3.0"; }
 
 usage () { echo "
     --version -- Prints the SDLF version
