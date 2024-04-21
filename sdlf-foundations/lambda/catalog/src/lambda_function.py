b2be10c073af9f92b37a6301acacce075f114f4e
Cyril Fait <cfait@misdre.info>
2024-04-21 18:21:13 +0200

    use proper DynamoDB JSON structure for items in sdlf-foundations catalog lambda

--- sdlf-foundations/lambda/catalog/src/lambda_function.py
+++ sdlf-foundations/lambda/catalog/src/lambda_function.py
@@ -1,14 +1,14 @@
 import json
 import logging
 import os
-from datetime import datetime
+from datetime import UTC, datetime
 from urllib.parse import unquote_plus
 
 import boto3
 from botocore.config import Config
 from botocore.exceptions import ClientError
 
-session_config = Config(user_agent_extra="awssdlf/2.1.0")
+session_config = Config(user_agent_extra="awssdlf/2.2.0")
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -18,11 +18,11 @@ catalog_table = f"octagon-ObjectMetadata-{os.environ['ENV']}"
 
 def parse_s3_event(s3_event):
     return {
-        "bucket": s3_event["s3"]["bucket"]["name"],
-        "key": unquote_plus(s3_event["s3"]["object"]["key"]),
-        "size": s3_event["s3"]["object"]["size"],
-        "last_modified_date": s3_event["eventTime"].split(".")[0] + "+00:00",
-        "timestamp": int(round(datetime.now(datetime.UTC).timestamp() * 1000, 0)),
+        "bucket": {"S": s3_event["detail"]["bucket"]["name"]},
+        "key": {"S": unquote_plus(s3_event["detail"]["object"]["key"])},
+        "size": {"N": str(s3_event["detail"]["object"]["size"])},
+        "last_modified_date": {"S": s3_event["time"]},
+        "timestamp": {"N": str(int(round(datetime.now(UTC).timestamp() * 1000, 0)))},
     }
 
 
@@ -54,23 +54,23 @@ def delete_item(table, key):
 
 def lambda_handler(event, context):
     try:
-        logger.info("Received {} messages".format(len(event["Records"])))
+        logger.info(f"Received {len(event['Records'])} messages")
         for record in event["Records"]:
             logger.info("Parsing S3 Event")
-            # Handling S3 Multipart Upload
-            message = json.loads(record["body"])["Records"][0] if "eventName" not in event else record
-            operation = message["eventName"].split(":")[-1]
+            message = json.loads(record["body"])
+            operation = message["detail-type"]
+            bucket = message["detail"]["bucket"]["name"]
+            key = unquote_plus(message["detail"]["object"]["key"])
+            id = f"s3://{bucket}/{key}"
 
             logger.info(f"Performing Dynamo {operation} operation")
-            if operation in ["Delete", "DeleteMarkerCreated"]:
-                id = "s3://{}/{}".format(message["s3"]["bucket"]["name"], unquote_plus(message["s3"]["object"]["key"]))
+            if operation in ["Object Deleted"]:
                 delete_item(catalog_table, {"id": id})
             else:
                 item = parse_s3_event(message)
-                item["id"] = f"s3://{item['bucket']}/{item['key']}"
-                item["stage"] = item["bucket"].split("-")[-1]
+                item["id"] = {"S": id}
+                item["stage"] = {"S": bucket.split("-")[-1]}
                 put_item(catalog_table, item, "id")
-
     except Exception as e:
         logger.error("Fatal error", exc_info=True)
         raise e
