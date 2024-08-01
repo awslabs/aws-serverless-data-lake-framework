e92e71d3adb54709aa43e61830ebb39bf56cb550
Cyril Fait <cfait@misdre.info>
2024-08-01 10:41:55 +0200

    pin cfn-lint to <1 in github workflow
    also exclude deployspec.yaml which is not a fully-formed cloudformation template

--- validate.sh
+++ validate.sh
@@ -12,8 +12,9 @@ find . -type f \( -name '*.sh' -o -name '*.bash' -o -name '*.ksh' \) -print0 \
 | xargs -0 shellcheck -x --format gcc
 
 # cloudformation
-find . -type f -name '*.yaml' -print0 \
+find . -type f -name '*.yaml' ! -name 'deployspec.yaml' -print0 \
 | xargs -0 cfn-lint
+
 ## unfortunately cfn_nag doesn't support fn::foreach so we exclude files using it: https://github.com/stelligent/cfn_nag/issues/621
 find . -not \( -type f -name 'template-glue-job.yaml' -o -type f -name 'template-lambda-layer.yaml' \) -type f -name '*.yaml' -print0 \
 | xargs -0 -L 1 cfn_nag_scan --fail-on-warnings --ignore-fatal --deny-list-path .cfn-nag-deny-list.yml --input-path
