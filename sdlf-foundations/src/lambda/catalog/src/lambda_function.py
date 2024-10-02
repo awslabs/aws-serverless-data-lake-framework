24f7c3f4114ddcc73f4938ee4ccb8ae35ac67157
Cyril Fait <cfait@misdre.info>
2024-10-02 14:46:21 +0200

    update version to 2.6.0

--- sdlf-foundations/src/lambda/catalog/src/lambda_function.py
+++ sdlf-foundations/src/lambda/catalog/src/lambda_function.py
@@ -8,7 +8,7 @@ import boto3
 from botocore.config import Config
 from botocore.exceptions import ClientError
 
-session_config = Config(user_agent_extra="awssdlf/2.5.0")
+session_config = Config(user_agent_extra="awssdlf/2.6.0")
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
