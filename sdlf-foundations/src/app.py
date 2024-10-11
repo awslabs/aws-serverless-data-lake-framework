91405db013495d3c19d79f094f6e52f9c0bcfed0
Cyril Fait <cfait@misdre.info>
2024-10-11 15:10:02 +0200

    daiml

---
+++ sdlf-foundations/src/app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import importlib
+import os
+import aws_cdk as cdk
+
+from foundations import Foundations
+
+app = cdk.App()
+stack = cdk.Stack(app, "FoundationsStack")
+Foundations(stack, "foundations")
+
+app.synth()
