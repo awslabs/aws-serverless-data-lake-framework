565f3ea395d8ed2757e07a36450abe5cf4a83ca5
Cyril Fait <cfait@misdre.info>
2024-10-22 13:19:45 +0200

    rework sdlf-dataset deployspec
    to be more in line with the existing seedfarmer community
    
    the model where we build packages will be using a standard buildspec instead, in a future PR

---
+++ sdlf-dataset/src/app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import aws_cdk as cdk
+
+from dataset import Dataset
+
+app = cdk.App()
+
+lakeformation_cicd_stack = cdk.Stack(app, "LakeFormationDatasetStack")
+
+cdk.aws_lakeformation.CfnDataLakeSettings(
+    lakeformation_cicd_stack,
+    "rDataLakeSettings",
+    admins=[
+        cdk.aws_lakeformation.CfnDataLakeSettings.DataLakePrincipalProperty(
+            data_lake_principal_identifier=cdk.Fn.sub(lakeformation_cicd_stack.synthesizer.cloud_formation_execution_role_arn)
+        ),
+    ],
+    mutation_type="APPEND",
+)
+
+stack = cdk.Stack(app, "DatasetStack")
+stack.add_dependency(lakeformation_cicd_stack)
+Dataset(stack, "dataset")
+
+app.synth()
