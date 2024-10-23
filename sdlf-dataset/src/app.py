fe71505ec615a2a6918faa20dd63ae89e3a0a433
Cyril Fait <cfait@misdre.info>
2024-10-23 15:22:06 +0200

    [sdlf-dataset][cdk] avoid using a hardcoded stack name

--- sdlf-dataset/src/app.py
+++ sdlf-dataset/src/app.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
 
 import aws_cdk as cdk
+import os
 
 from dataset import Dataset
 
-app = cdk.App()
+# Project specific
+project_name = os.getenv("SEEDFARMER_PROJECT_NAME", "")
+deployment_name = os.getenv("SEEDFARMER_DEPLOYMENT_NAME", "")
+module_name = os.getenv("SEEDFARMER_MODULE_NAME", "")
 
-lakeformation_cicd_stack = cdk.Stack(app, "LakeFormationDatasetStack")
+if project_name:
+    stack_name = f"{project_name}-{deployment_name}-{module_name}"
+else:  # app.py not used in a seedfarmer context somehow
+    stack_name = "sdlf-dataset"
+
+app = cdk.App()
+lakeformation_cicd_stack = cdk.Stack(app, f"{stack_name}-lakeformation")
 
 cdk.aws_lakeformation.CfnDataLakeSettings(
     lakeformation_cicd_stack,
@@ -21,7 +31,7 @@ cdk.aws_lakeformation.CfnDataLakeSettings(
     mutation_type="APPEND",
 )
 
-stack = cdk.Stack(app, "DatasetStack")
+stack = cdk.Stack(app, stack_name)
 stack.add_dependency(lakeformation_cicd_stack)
 Dataset(stack, "dataset")
 
