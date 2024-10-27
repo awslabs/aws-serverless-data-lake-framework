cbf3f748d169a7354b51260c33e65d2accfbc986
Cyril Fait <cfait@misdre.info>
2024-10-27 19:34:19 +0100

    [sdlf-foundations][cdk] ability to provide parameter values without --parameters

--- sdlf-foundations/src/app.py
+++ sdlf-foundations/src/app.py
@@ -12,12 +12,25 @@ module_name = os.getenv("SEEDFARMER_MODULE_NAME", "")
 
 if project_name:
     stack_name = f"{project_name}-{deployment_name}-{module_name}"
+    param_prefix = "SEEDFARMER_PARAMETER_"
 else:  # app.py not used in a seedfarmer context somehow
     stack_name = "sdlf-foundations"
+    param_prefix = ""
+
+
+def _param(name: str, default: str = None) -> str:
+    return os.getenv(f"{param_prefix}{name}", default)
+
 
 app = cdk.App()
 stack = cdk.Stack(app, stack_name)
-stack_foundations = Foundations(stack, "foundations")
+stack_foundations = Foundations(
+    stack,
+    "foundations",
+    org=_param("ORG"),
+    data_domain=_param("DATA_DOMAIN"),
+    account_id=os.getenv("CDK_DEFAULT_ACCOUNT"),
+)
 
 cdk.CfnOutput(
     scope=stack,
