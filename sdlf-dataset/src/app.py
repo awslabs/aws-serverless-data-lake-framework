557c0f689dbf1181cf8e60b64d3fec6caf47c2eb
Cyril Fait <cfait@misdre.info>
2024-10-28 13:51:35 +0100

    [sdlf-dataset][cdk] ability to provide parameter values without CfnParameter

--- sdlf-dataset/src/app.py
+++ sdlf-dataset/src/app.py
@@ -12,8 +12,15 @@ module_name = os.getenv("SEEDFARMER_MODULE_NAME", "")
 
 if project_name:
     stack_name = f"{project_name}-{deployment_name}-{module_name}"
+    param_prefix = "SEEDFARMER_PARAMETER_"
 else:  # app.py not used in a seedfarmer context somehow
     stack_name = "sdlf-dataset"
+    param_prefix = ""
+
+
+def _param(name: str, default: str = None) -> str:
+    return os.getenv(f"{param_prefix}{name}", default)
+
 
 app = cdk.App()
 lakeformation_cicd_stack = cdk.Stack(app, f"{stack_name}-lakeformation")
@@ -33,7 +40,17 @@ cdk.aws_lakeformation.CfnDataLakeSettings(
 
 stack = cdk.Stack(app, stack_name)
 stack.add_dependency(lakeformation_cicd_stack)
-stack_dataset = Dataset(stack, "dataset")
+stack_dataset = Dataset(
+    stack,
+    "dataset",
+    raw_bucket=_param("RAW_BUCKET"),
+    stage_bucket=_param("STAGE_BUCKET"),
+    analytics_bucket=_param("ANALYTICS_BUCKET"),
+    artifacts_bucket=_param("ARTIFACTS_BUCKET"),
+    lakeformation_dataaccess_role=_param("LAKEFORMATION_DATAACCESS_ROLE"),
+    dataset=_param("DATASET"),
+    s3_prefix=_param("S3_PREFIX"),
+)
 
 cdk.CfnOutput(
     scope=stack,
