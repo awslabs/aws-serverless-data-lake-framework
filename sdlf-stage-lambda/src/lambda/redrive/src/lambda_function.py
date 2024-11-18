1cb60bbd6a4b2c0d43707559fd7f225fa8bb562b
Cyril Fait <cfait@misdre.info>
2024-11-18 18:26:07 +0100

    [sdlf-stage-lambda] pass instance parameters to datalakeLibrary

--- sdlf-stage-lambda/src/lambda/redrive/src/lambda_function.py
+++ sdlf-stage-lambda/src/lambda/redrive/src/lambda_function.py
@@ -1,23 +1,23 @@
 import os
 
 from datalake_library.commons import init_logger
-from datalake_library.configuration.resource_configs import SQSConfiguration
+from datalake_library.sdlf import SQSConfiguration
 from datalake_library.interfaces.sqs_interface import SQSInterface
 
 logger = init_logger(__name__)
-
+deployment_instance = os.environ["DEPLOYMENT_INSTANCE"]
 
 def lambda_handler(event, context):
     try:
-        sqs_config = SQSConfiguration(os.environ["TEAM"], os.environ["PIPELINE"], os.environ["STAGE"])
-        dlq_interface = SQSInterface(sqs_config.get_stage_dlq_name)
+        sqs_config = SQSConfiguration(instance=deployment_instance)
+        dlq_interface = SQSInterface(sqs_config.stage_dlq)
         messages = dlq_interface.receive_messages(1)
         if not messages:
             logger.info("No messages found in {}".format(sqs_config.get_stage_dlq_name))
             return
 
         logger.info("Received {} messages".format(len(messages)))
-        queue_interface = SQSInterface(sqs_config.get_stage_queue_name)
+        queue_interface = SQSInterface(sqs_config.stage_queue)
         for message in messages:
             queue_interface.send_message_to_fifo_queue(message["Body"], "redrive")
             logger.info("Redrive message succeeded")
